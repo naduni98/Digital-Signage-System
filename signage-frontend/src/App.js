b8c6be800f1f0b99945af859c9850859d12bfc39
NADU\nadun <it20649708@my.sliit.lk>
2025-07-12 01:38:18 +0530

    typo.

--- signage-frontend/src/App.js
+++ signage-frontend/src/App.js
@@ -1,17 +1,17 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+
 import AppRoutes from './routes/AppRoutes';
 import { useAutoLogout } from './features/auth/hooks/useAutoLogout';
 
 function App() {
   useAutoLogout();
   return (
-    
-    <BrowserRouter>
+   
    
       <AppRoutes />
-    </BrowserRouter>
+
   );
 }
 
