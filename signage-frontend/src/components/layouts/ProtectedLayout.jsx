290a834062240725ea57b03806dcfd618a9ecfd1
NADU\nadun <it20649708@my.sliit.lk>
2025-07-12 12:43:19 +0530

    Complete Header.

--- signage-frontend/src/components/layouts/ProtectedLayout.jsx
+++ signage-frontend/src/components/layouts/ProtectedLayout.jsx
@@ -1,12 +1,12 @@
 // src/components/layouts/ProtectedLayout.jsx
 import React from 'react';
-import Header from '../common/Header';
+import Header from './Header';
 
 export default function ProtectedLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 bg-gray-50 p-4">
+      <main className="flex-1 bg-gray-50 p-4 px-32">
         {children}
       </main>
     </div>
